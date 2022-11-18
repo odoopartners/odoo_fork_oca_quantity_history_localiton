4cab23af2327ada93a3ab86e16e728131dff9a72
bragonzu <bragonzu@hotmail.com>
2022-11-18 11:56:48 -0500

    [IMP] added assets in manifest

--- fork_stock_quantity_history_location/__manifest__.py
+++ fork_stock_quantity_history_location/__manifest__.py
@@ -5,10 +5,16 @@
 {
     "name": "Stock Quantity History Location",
     "summary": "Provides stock quantity by location on past date",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "license": "AGPL-3",
     "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "depends": ["stock"],
+    "assets": {
+                'web.assets_backend': [
+                    'fork_stock_quantity_history_location/static/src/js/inventory_report.js',
+                ],
+                },
     "data": ["wizards/stock_quantity_history.xml"],
+
 }
